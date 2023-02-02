f305b66bef2f7f9db12c32389ea8f8b5bbccf72e
fabfreund <fafreund_popgen@gmx.net>
2023-02-02 14:00:22 +0100

    reordered plots, added data refs, ...

---
+++ example_fasta/README.md
@@ -0,0 +1,8 @@
+Data sources:
+
+ * Aptenodytes patagonicus: J. Romiguier, P. Gayral, M. Ballenghien, A. Bernard, V. Cahais, A. Chenuil, Y. Chiari, R. Der-
+nat, L. Duret, N. Faivre, and et al. Comparative population genomics in animals uncovers the
+determinants of genetic diversity. Nature, 515(7526):261–263, Aug 2014.
+ * Aptenodytes forsteri: Emeric Figuet, Marion Ballenghien, Jonathan Romiguier, and Nicolas Galtier. Biased Gene
+Conversion and GC-Content Evolution in the Coding Sequences of Reptiles and Vertebrates.
+Genome Biology and Evolution, 7(1):240–250, 12 2014.
--- main_dataanalysis/fits_pic.R
+++ main_dataanalysis/fits_pic.R
@@ -1,11 +1,10 @@
 #' Plot the fits of the best-fitting 
 #' models from KM, BETA, PSI. 
 #' as well as observed SFS and GC-bias corrected SFS
-#' Plots are ordered by fit measured by 
-#' Cohen's d
+#' Plots are ordered alphabetically
 
 #devanil transforms the SFS by multiplying its ith entry with i
-devanil <- TRUE#FALSE
+devanil <- FALSE
 
 end1 <- ifelse(devanil,"tf","")
 
@@ -23,28 +22,26 @@ rename <- function(name1){paste0("../sfs_data/gcbias_corrected_sfs/",strsplit(na
 pdf(paste0("pics/fits",end1,".pdf"))
 mar.default <- par()$mar
 par(mar = mar.default + c(0, 1, 0, 0)) 
-k <- 0
-for (name1 in ml_bf_eff$file){
-temp1 <- read.table(rename(name1))
+for (k in order(ml_bf_eff$species)){
+temp1 <- read.table(rename(ml_bf_eff$file[k]))
 neutralsfs <- temp1[,2]/sum(temp1[,2])
 if (devanil){neutralsfs <- devanilla(neutralsfs)}
-k <- k+1 
-xvals <- seq(along=sfs_list[[name1]][["obs"]])
+xvals <- seq(along=sfs_list[[ml_bf_eff$file[k]]][["obs"]])
 if (devanil){
 for (mod1 in c("obs","km","beta","psi")){
-sfs_list[[name1]][[mod1]] <- devanilla(
-  sfs_list[[name1]][[mod1]])}}
-range1 <- range(unlist(sfs_list[[name1]]))
+sfs_list[[ml_bf_eff$file[k]]][[mod1]] <- devanilla(
+  sfs_list[[ml_bf_eff$file[k]]][[mod1]])}}
+range1 <- range(unlist(sfs_list[[ml_bf_eff$file[k]]]))
 range1 <- range(range1,neutralsfs)
 
 if (!devanil){
-plot(xvals,sfs_list[[name1]][["obs"]],
+plot(xvals,sfs_list[[ml_bf_eff$file[k]]][["obs"]],
      ylim=range1,pch=20,
        ylab=expression(E(xi[i])/sum(E(xi))),
        xlab="SFS classes",
      main=ml_bf_eff$species[k]
      )} else {
-       plot(xvals,sfs_list[[name1]][["obs"]],
+       plot(xvals,sfs_list[[ml_bf_eff$file[k]]][["obs"]],
             ylim=range1,pch=20,
             ylab=expression(i*E(xi[i])/sum(E(xi))),
             xlab="SFS classes",
@@ -62,26 +59,25 @@ effsize <- switch(ml_bf_eff$selected_model[k],
                   "MMC"=min(ml_bf_eff[k,8],
                             ml_bf_eff[k,15]))
 effsize <- round(effsize,2)
-points(xvals,sfs_list[[name1]][["km"]],
+points(xvals,sfs_list[[ml_bf_eff$file[k]]][["km"]],
        pch=20,col="black",
        type="l",lty=ltys[1])
-points(xvals,sfs_list[[name1]][["beta"]],
+points(xvals,sfs_list[[ml_bf_eff$file[k]]][["beta"]],
        pch=20,col="blue",
        type="l",lty=ltys[2])
-points(xvals,sfs_list[[name1]][["psi"]],
+points(xvals,sfs_list[[ml_bf_eff$file[k]]][["psi"]],
        pch=20,col="red",
        type="l",lty=ltys[3])
 points(xvals,neutralsfs,
-       pch=4,col="gray",
+       pch=4,col="darkgray",
        type="p")
 legend("topright",legend=c("Observed","GC-bias corrected",
 paste0("ML KM: (",paste0(unname(ml_bf_eff[k,18:19]),collapse = ","),")"),
 paste0("ML Beta: (",paste0(unname(ml_bf_eff[k,3:5]),collapse = ","),")"),
 paste0("ML PSI: (",paste0(unname(ml_bf_eff[k,10:12]),collapse = ","),")")),
 pch=c(20,4,NA,NA,NA),
-col=c("black","gray","black","blue","red"),
+col=c("black","darkgray","black","blue","red"),
 lty=c(NA,NA,ltys))
 text(max(xvals)/4,range1[2],paste("Cramer's d:",effsize))         
   }
 dev.off()
-
